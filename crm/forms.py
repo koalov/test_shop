fbc6f700e85d3c176f3144f2bc817f03ba0cdf38
Kostyantyn Ovcharuk <koalov99@gmail.com>
2022-09-05 01:24:29 +0300

    Refactor some templates and carusel, price and base.html were added

---
+++ crm/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class OrderForm(forms.Form):
+    name = forms.CharField(max_length=150,
+                           widget=forms.TextInput(attrs={'class': 'css_input'}))
+    phone = forms.CharField(max_length=15)
